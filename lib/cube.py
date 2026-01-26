dc8b4f1d3fc4e01eee9f15989bc09d9a6d3d4b46
Christopher Schmitz <schmitz.chris@gmail.com>
2026-01-26 09:04:32 -0600

    WIP Adds more tests, installs pytest

--- lib/cube.py
+++ lib/cube.py
@@ -26,11 +26,12 @@ class Cell:
     def __init__(self, face_position: str, led_index: int):
         self.face_position = face_position
         self.index = led_index
-        self.current_color = (0,0,0)
+        self.current_color = (0, 0, 0)
         # self.current_color = color.BLACK
 
     def set_color(self, new_color):
         self.current_color = new_color
+        return self
 
     def get_led_index(self):
         return self.index
@@ -38,34 +39,39 @@ class Cell:
     def get_current_color(self):
         return self.current_color
 
+    def get_face_position(self):
+        return self.face_position
+
 
 class CubeFace:
     def __init__(self, face: str, cells: List[Cell]):  # cells: List[Cell])
         self.face = face
-        self.cells: Dict[str, Cell] = {}
+        self.cells_face_position_map: Dict[str, Cell] = {}
         for cell in cells:
-            self.cells[cell.face_position] = cell
+            self.cells_face_position_map[cell.face_position] = cell
         # self.cells = cells
 
     def get_name(self):
         return self.face
 
-    def fill(self, color, pixels):  # color: Tuple[int, int, int]
-        for cell in self.cells:
-            pixels[cell.index] = color
+    # todo: this needs to be updated. the cell method name is different and we to pull the face by it's mapping
+    def fill(self, color: Tuple[int, int, int]):
+        for cell in self.cells_face_position_map:
             cell.set_color(color)
 
     def set_cell_color(self, position: FacePosition, color: Tuple[int, int, int]):
-        print(self.cells)
-        self.cells[position].set_color(color)
+        print(self.cells_face_position_map)
+        self.cells_face_position_map[position].set_color(color)
 
     def get_cells(self):
-        return self.cells
+        return self.cells_face_position_map
+
+    def get_state(self):
+        return [cell for _, cell in self.cells_face_position_map.items()]
 
 
 class Cube:
-    def __init__(self,pixels:List[Tuple[int,int,int]], faces: List[CubeFace] ):
-        self.pixels = pixels
+    def __init__(self, faces: List[CubeFace]):
         self.faces = {}
         for face in faces:
             self.faces[face.get_name()] = face
@@ -77,20 +83,22 @@ class Cube:
         print(face_to_rotate)
         face = self.faces
 
-    def set_face_color(self, face: Face, color: Tuple[int,int,int]):
-      face = self._get_face(face)
-      face.fill(color, self.pixels)
+    def set_face_color(self, face: Face, color: Tuple[int, int, int]):
+        face = self._get_face(face)
 
-    def set_cell_color(self, face: Face, position: FacePosition, color: Tuple[int,int,int]):
+    def set_cell_color(self, face: Face, position: FacePosition, color: Tuple[int, int, int]):
         self._get_face(face).set_cell_color(position, color)
 
     def get_state(self):
         # todo: the cube has to know a lot about the cells here. is there a way we can move some of the logic up into the CubeFace?
         top_cells = [face.get_cells() for _, face in self.faces.items()][0]
         # todo: hmmmm, maybe it's worth writing out a for loop, at least for this first level since we can't do an actual functional pipeline?
-        # top_cells = self.faces[Face.TOP].get_cells()
-        index_and_color = [{'index': cell.get_led_index(), 'color': cell.get_current_color()} for _, cell in top_cells.items()]
+        # ? do we really need this remapping of the cell to a dict? can't we just sort the cells?
+        index_and_color = [{'index': cell.get_led_index(), 'color': cell.get_current_color()} for _, cell in
+                           top_cells.items()]
         sorted_list = sorted(index_and_color, key=lambda c: c['index'])
+        highest_led_index = sorted_list[-1]['index']
+        colors_in_order = [sorted_list[i] or (0, 0, 0) for i in range(highest_led_index)]
         return [cell['color'] for cell in sorted_list]
 
 # {0, 1, 2, 3, 6, 8, 11, 13, 16, 18}
--- tests/cube_test.py
+++ tests/cube_test.py
@@ -1,49 +1,140 @@
 import unittest
+import pytest
 
 from lib.cube import Cube, CubeFace, Face, FacePosition, Cell
+import json
 
 
-class CubeTest(unittest.TestCase):
-    def test_can_fill_a_face_with_a_color(self):
-        pixels = [0] * 53
-        cells = [
-                Cell(FacePosition.TOP_LEFT, 0),
-                Cell(FacePosition.TOP_CENTER, 1),
+class TestCubeFace:
+    def test_can_get_cells_without_face_position_outer_map(self):
+        # * we're storing the cells in a map according to their face position. This test just makes sure we can return
+        # * just the cells when generating the overall state for the cube.
+        face = CubeFace(
+            Face.LEFT,
+            [
+                Cell(FacePosition.TOP_LEFT, 5),
+                Cell(FacePosition.TOP_CENTER, 8),
                 Cell(FacePosition.TOP_RIGHT, 2),
             ]
-        cube = Cube(pixels,[
-            CubeFace(Face.TOP, cells)
-        ] )
+        )
+        face.set_cell_color(FacePosition.TOP_LEFT, (255, 0, 255))
+        face.set_cell_color(FacePosition.TOP_RIGHT, (255, 0, 255))
+        face.set_cell_color(FacePosition.TOP_CENTER, (255, 0, 255))
 
-        cube.set_face_color(Face.TOP, (255,255,255))
+        actual = face.get_state()
+
+        assert [
+                   serialize_cell(Cell(FacePosition.TOP_LEFT, 5).set_color((255, 0, 255))),
+                   serialize_cell(Cell(FacePosition.TOP_CENTER, 8).set_color((255, 0, 255))),
+                   serialize_cell(Cell(FacePosition.TOP_RIGHT, 2).set_color((255, 0, 255))),
+               ] == [serialize_cell(cell) for cell in actual]
+
+
+class TestCube:
+    def test_can_fill_a_face_with_a_color(self):
+        cells = [
+            Cell(FacePosition.TOP_LEFT, 0),
+            Cell(FacePosition.TOP_CENTER, 1),
+            Cell(FacePosition.TOP_RIGHT, 2),
+        ]
+        cube = Cube([
+            CubeFace(Face.TOP, cells)
+        ])
+
+        cube.set_face_color(Face.TOP, (255, 255, 255))
 
         for cell in cells:
-            self.assertEquals(pixels[cell.get_led_index()], (255,255,255))
-            self.assertEquals(cell.get_current_color(), (255,255,255))
+            assert cell.get_current_color() == (255, 255, 255)
 
-    def test_can_set_specific_cell_colors(self):
-        pixels = []
+    def test_cells_start_off_with_no_color(self):
         top_cells = [
             Cell(FacePosition.TOP_RIGHT, 2),
             Cell(FacePosition.TOP_LEFT, 0),
             Cell(FacePosition.TOP_CENTER, 1),
         ]
-        # todo: if we're changing the logic to be "give me the new state and I'll take that and set all of the
-        #   pixel values externally, then we can remove pixels from this class
-        cube = Cube(pixels, [
+        cube = Cube([
             CubeFace(Face.TOP, top_cells)
         ])
 
-        cube.set_cell_color(Face.TOP, FacePosition.TOP_LEFT, (255,0,0))
-        cube.set_cell_color(Face.TOP, FacePosition.TOP_CENTER, (0,0,255))
-        cube.set_cell_color(Face.TOP, FacePosition.TOP_RIGHT, (0,255,0))
         actual = cube.get_state()
 
-        self.assertEqual([(255,0,0), (0,0,255), (0,255,0)], actual)
+        assert [(0, 0, 0), (0, 0, 0), (0, 0, 0)] == actual
+
+    def test_can_set_specific_cell_colors_on_a_single_face_top(self):
+        top_cells = [
+            Cell(FacePosition.TOP_RIGHT, 2),
+            Cell(FacePosition.TOP_LEFT, 0),
+            Cell(FacePosition.TOP_CENTER, 1),
+        ]
+        cube = Cube([
+            CubeFace(Face.TOP, top_cells)
+        ])
+
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_LEFT, (255, 0, 0))
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_CENTER, (0, 0, 255))
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_RIGHT, (0, 255, 0))
+        actual = cube.get_state()
+
+        assert [(255, 0, 0), (0, 0, 255), (0, 255, 0)] == actual
+
+    def test_cells_that_havent_had_their_colors_changed_remain_same_color(self):
+        top_cells = [
+            Cell(FacePosition.TOP_RIGHT, 2),
+            Cell(FacePosition.TOP_LEFT, 0),
+            Cell(FacePosition.TOP_CENTER, 1),
+        ]
+        cube = Cube([
+            CubeFace(Face.TOP, top_cells),
+        ])
+        # * same setup as the last test, but we're making all fo the colors consistent so the
+        # * changed one stands out better visually in the result
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_LEFT, (255, 0, 0))
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_CENTER, (255, 0, 0))
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_RIGHT, (255, 0, 0))
+        colors_set = cube.get_state()
+        # * so confirming, all of the cells are set to red
+        assert [(255, 0, 0), (255, 0, 0), (255, 0, 0)] == colors_set
+
+        # * and now we change only one
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_CENTER, (255, 255, 255))
+        actual = cube.get_state()
+
+        assert [(255, 0, 0), (255, 255, 255), (255, 0, 0)] == actual
+
+    def test_state_output_respectes_gaps_between_cells_as_far_as_assigned_led_indexes_go(self):
+        # ^ :exhausted: I don't know the best way to word the naming of this test, but basically
+        # ^ the cube class structure allows us to skip led_indexes. We _wouldn't_, but we _could_.
+        # ^ So, best to make sure if we _do_ skip some indexes we still output the cube state with
+        # ^ the correct cell -> led_index order. So if there's a gap between assigned led_indexes to
+        # ^ cells, those gapped led_indexes would still be included in the output state and their color
+        # ^ would be black.
+        top_cells = [
+            Cell(FacePosition.TOP_RIGHT, 5),
+            Cell(FacePosition.TOP_LEFT, 0),
+        ]
+        cube = Cube([
+            CubeFace(Face.TOP, top_cells),
+        ])
+        # * same setup as the last test, but we're making all fo the colors consistent so the
+        # * changed one stands out better visually in the result
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_LEFT, (255, 0, 0))
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_RIGHT, (255, 0, 0))
+
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_LEFT, (255, 255, 255))
+        cube.set_cell_color(Face.TOP, FacePosition.TOP_RIGHT, (255, 255, 255))
+        actual = cube.get_state()
+
+        assert [
+                   (255, 255, 255),
+                   (0, 0, 0),
+                   (0, 0, 0),
+                   (0, 0, 0),
+                   (0, 0, 0),
+                   (255, 255, 255)
+               ] == actual
 
     def test_can_rotate(self):
-        pass
-        pixels = [0] * 53
+        pass  # ! gotta get other methods working first
         # * Note that for the orientation of the cells:
         # - the "top" of all faces other than the top or bottom is relative to the TOP face
         #     - e.g. the top row of the left face is adjacent to the TOP face, the top row of the right face is adjacent to the top
@@ -59,7 +150,7 @@ class CubeTest(unittest.TestCase):
             Cell(FacePosition.MIDDLE_LEFT, 4),
             Cell(FacePosition.BOTTOM_LEFT, 5),
         ]
-        cube = Cube(pixels, [
+        cube = Cube([
             CubeFace(Face.TOP, top_cells),
             CubeFace(Face.LEFT, left_cells)
         ])
@@ -69,3 +160,12 @@ class CubeTest(unittest.TestCase):
         # ^ https://jperm.net/3x3/moves
         cube.rotate()
 
+
+# * === helpers ===
+# ? circuit python doesn't allow for @dataclass (not practically) and really I only need serialization for state
+# ? comparison in testing, so I don't want to put a bunch of serialization methods into the classes just for test
+# ? assertions. That combined with the fact that this is a super tiny codebase and this file will likely be the only
+# ? set of test suites I'm fine with just making hyper specific serialization helper functions for each of the classes.
+# ? That way we're not comparing class instant ids and when there's a test failure we can actually read the dif.
+def serialize_cell(cell: Cell):
+    return f'position:{cell.get_face_position()}, led_index: {cell.get_led_index()}, color: {cell.get_current_color()}'
