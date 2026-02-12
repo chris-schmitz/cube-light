7c89e60089ee13604a4644fa96b347f45a47955e
Christopher Schmitz <schmitz.chris@gmail.com>
2026-02-12 09:01:16 -0600

    Adds correct face position assignments

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from typing import List, Tuple
 
 import board
 import neopixel
-from lib.cube import Cell, CubeFace, Cube, LedAssignmentData, Face, FacePosition
+from lib.cube import Cell, CubeFace, Cube, LedAssignmentData, Face, FacePosition, Rotations
 
 TOTAL_PIXELS = 54
-pixels = neopixel.NeoPixel(board.D5, TOTAL_PIXELS, brightness=1.0, auto_write=False)
+pixels = neopixel.NeoPixel(
+    board.D5,
+    TOTAL_PIXELS,
+    pixel_order=neopixel.RGB,
+    brightness=1.0,
+    auto_write=False
+)
 
 top = CubeFace(Face.TOP, [
     Cell(FacePosition.TOP_LEFT, 8),
     Cell(FacePosition.TOP_CENTER, 11),
     Cell(FacePosition.TOP_RIGHT, 13),
+
     Cell(FacePosition.MIDDLE_LEFT, 6),
     Cell(FacePosition.MIDDLE_CENTER, 0),
     Cell(FacePosition.MIDDLE_RIGHT, 16),
+
     Cell(FacePosition.BOTTOM_LEFT, 3),
     Cell(FacePosition.BOTTOM_CENTER, 1),
     Cell(FacePosition.BOTTOM_RIGHT, 18),
@@ -23,9 +31,11 @@ left = CubeFace(Face.LEFT, [
     Cell(FacePosition.TOP_LEFT, 9),
     Cell(FacePosition.TOP_CENTER, 7),
     Cell(FacePosition.TOP_RIGHT, 5),
+
     Cell(FacePosition.MIDDLE_LEFT, 30),
     Cell(FacePosition.MIDDLE_CENTER, 29),
     Cell(FacePosition.MIDDLE_RIGHT, 28),
+
     Cell(FacePosition.BOTTOM_LEFT, 48),
     Cell(FacePosition.BOTTOM_CENTER, 52),
     Cell(FacePosition.BOTTOM_RIGHT, 33),
@@ -35,44 +45,52 @@ bottom = CubeFace(Face.BOTTOM, cells=[
     Cell(FacePosition.TOP_LEFT, 34),
     Cell(FacePosition.TOP_CENTER, 37),
     Cell(FacePosition.TOP_RIGHT, 39),
-    Cell(FacePosition.MIDDLE_LEFT, 42),
-    Cell(FacePosition.MIDDLE_CENTER, 44),
-    Cell(FacePosition.MIDDLE_RIGHT, 47),
+
+    Cell(FacePosition.MIDDLE_LEFT, 51),
+    Cell(FacePosition.MIDDLE_CENTER, 53),
+    Cell(FacePosition.MIDDLE_RIGHT, 42),
+
     Cell(FacePosition.BOTTOM_LEFT, 50),
-    Cell(FacePosition.BOTTOM_CENTER, 51),
-    Cell(FacePosition.BOTTOM_RIGHT, 53),
+    Cell(FacePosition.BOTTOM_CENTER, 47),
+    Cell(FacePosition.BOTTOM_RIGHT, 44),
 ])
 front = CubeFace(Face.FRONT, cells=[
-    Cell(FacePosition.TOP_LEFT, 2),
-    Cell(FacePosition.TOP_CENTER, 4),
+    Cell(FacePosition.TOP_LEFT, 4),
+    Cell(FacePosition.TOP_CENTER, 2),
     Cell(FacePosition.TOP_RIGHT, 20),
-    Cell(FacePosition.MIDDLE_LEFT, 25),
+
+    Cell(FacePosition.MIDDLE_LEFT, 27),
+    Cell(FacePosition.MIDDLE_RIGHT, 25),
     Cell(FacePosition.MIDDLE_CENTER, 26),
-    Cell(FacePosition.MIDDLE_RIGHT, 27),
+
     Cell(FacePosition.BOTTOM_LEFT, 35),
     Cell(FacePosition.BOTTOM_CENTER, 36),
     Cell(FacePosition.BOTTOM_RIGHT, 38),
 ])
 
 back = CubeFace(Face.BACK, cells=[
-    Cell(FacePosition.TOP_LEFT, 10),
-    Cell(FacePosition.TOP_CENTER, 12),
-    Cell(FacePosition.TOP_RIGHT, 14),
-    Cell(FacePosition.MIDDLE_LEFT, 21),
-    Cell(FacePosition.MIDDLE_CENTER, 31),
-    Cell(FacePosition.MIDDLE_RIGHT, 32),
-    Cell(FacePosition.BOTTOM_LEFT, 45),
-    Cell(FacePosition.BOTTOM_CENTER, 46),
-    Cell(FacePosition.BOTTOM_RIGHT, 49),
+    Cell(FacePosition.TOP_LEFT, 49),
+    Cell(FacePosition.TOP_CENTER, 46),
+    Cell(FacePosition.TOP_RIGHT, 45),
+
+    Cell(FacePosition.MIDDLE_LEFT, 31),
+    Cell(FacePosition.MIDDLE_CENTER, 32),
+    Cell(FacePosition.MIDDLE_RIGHT, 21),
+
+    Cell(FacePosition.BOTTOM_LEFT, 10),
+    Cell(FacePosition.BOTTOM_CENTER, 12),
+    Cell(FacePosition.BOTTOM_RIGHT, 14),
 ])
 
 right = CubeFace(Face.RIGHT, cells=[
-    Cell(FacePosition.TOP_LEFT, 15),
+    Cell(FacePosition.TOP_LEFT, 19),
+    Cell(FacePosition.TOP_RIGHT, 15),
     Cell(FacePosition.TOP_CENTER, 17),
-    Cell(FacePosition.TOP_RIGHT, 19),
-    Cell(FacePosition.MIDDLE_LEFT, 22),
+
+    Cell(FacePosition.MIDDLE_LEFT, 24),
     Cell(FacePosition.MIDDLE_CENTER, 23),
-    Cell(FacePosition.MIDDLE_RIGHT, 24),
+    Cell(FacePosition.MIDDLE_RIGHT, 22),
+
     Cell(FacePosition.BOTTOM_LEFT, 40),
     Cell(FacePosition.BOTTOM_CENTER, 41),
     Cell(FacePosition.BOTTOM_RIGHT, 43),
@@ -93,7 +111,7 @@ def update_pixels(strip_colors):
 yellow = (255, 255, 0)
 blue = (0, 0, 255)
 red = (255, 0, 0)
-orange = (255, 165, 0)
+orange = (255, 98, 0)
 green = (0, 255, 0)
 white = (200, 200, 200)
 face_color_choices = [yellow, blue, red, orange, green, white]
@@ -123,7 +141,7 @@ def get_face_colors():
 
 get_face_colors()
 while True:
-    cube.rotate(Face.FRONT)
+    cube.rotate(Rotations.R)
     state = cube.get_state()
 
     if current_state != state:
